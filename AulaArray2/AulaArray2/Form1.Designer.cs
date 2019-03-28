3226cf94db9be4c71117da469c124f5ea52b3d17
Jonas Valereo <jvalereo2016@gmail.com>
2019-03-28 14:36:49 -0300

    Programa Teste Array Vetor e Texto
    Projeto, Programa teste em C#, com Array em Vetor e Texto

---
+++ AulaArray2/AulaArray2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace AulaArray2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.BtnVetor = new System.Windows.Forms.Button();
+            this.BtnTexto = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(80, 39);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(152, 22);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Testando Array";
+            this.lblTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // BtnVetor
+            // 
+            this.BtnVetor.Location = new System.Drawing.Point(100, 124);
+            this.BtnVetor.Name = "BtnVetor";
+            this.BtnVetor.Size = new System.Drawing.Size(114, 40);
+            this.BtnVetor.TabIndex = 1;
+            this.BtnVetor.Text = "Teste Vetor";
+            this.BtnVetor.UseVisualStyleBackColor = true;
+            this.BtnVetor.Click += new System.EventHandler(this.BtnVetor_Click);
+            // 
+            // BtnTexto
+            // 
+            this.BtnTexto.Location = new System.Drawing.Point(100, 200);
+            this.BtnTexto.Name = "BtnTexto";
+            this.BtnTexto.Size = new System.Drawing.Size(114, 41);
+            this.BtnTexto.TabIndex = 2;
+            this.BtnTexto.Text = "Vetor Texto";
+            this.BtnTexto.UseVisualStyleBackColor = true;
+            this.BtnTexto.Click += new System.EventHandler(this.BtnTexto_Click);
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Location = new System.Drawing.Point(120, 275);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(70, 33);
+            this.BtnSair.TabIndex = 3;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(301, 336);
+            this.ControlBox = false;
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.BtnTexto);
+            this.Controls.Add(this.BtnVetor);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teste Array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button BtnVetor;
+        private System.Windows.Forms.Button BtnTexto;
+        private System.Windows.Forms.Button BtnSair;
+    }
+}
+
---
+++ AulaArray2/AulaArray2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaArray2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnVetor_Click(object sender, EventArgs e)
+        {
+            // Aula teste Array e vetor
+            // Jonas Valereo Técnico em Informática
+
+            //Declarando o codigo Array e instanciando o vetor tipo int 
+
+            int [] vetor = new int [4];
+
+            vetor[0] = 7;
+            vetor[1] = 3;
+            vetor[2] = 5;
+            vetor[3] = 1;
+
+            //Declarando a variavel valor para acessar o vetor
+
+            int valor = vetor[1];
+            System.Array.Sort(vetor);
+
+            //Declarando o codigo for , percorre o veto em cada indice
+
+            for (int index = 0; index < 4; index++)
+            MessageBox.Show(vetor[index].ToString(), "Array");
+
+            //Fim do programa 
+        }
+
+        private void BtnTexto_Click(object sender, EventArgs e)
+        {
+            //Declarando o Array e instanciando o vetor semana tipo string
+
+            string[] semana = new string[7];
+
+            semana[0] = "Domingo";
+            semana[1] = "Segunda-feira";
+            semana[2] = "Tercça-feira";
+            semana[3] = "Quarta-feira";
+            semana[4] = "Quinta-feira";
+            semana[5] = "Sexta-feira";
+            semana[6] = "Sábado";
+
+            //Declranao a variavel valor para acessar o vetor semana
+
+            string valor = semana[1];
+            
+            //Declarando o codigo for, para percorre em cada indice 
+
+            for (int index = 0; index < 7; index++)
+                MessageBox.Show(semana[index].ToString(), "Array");
+
+            //Fim do programa
+
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            //Declarando o codigo sair close
+
+            Close();
+
+        }
+    }
+}
